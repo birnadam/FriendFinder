cffe6a4fdb36110bf3da8a4c9632f7c0a60b0249
birna <briandam26@yahoo.com>
2019-07-08 18:52:47 -0700

    cleaned up the code, finished up logic so modal pops up with the friend

---
+++ app/data/friends.js
@@ -0,0 +1,101 @@
+var friends = [
+    {
+      name: "Midoriya",
+      photo: "https://pbs.twimg.com/profile_images/968698422751518720/zeP0peZp.jpg",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Bakugo",
+      photo: "https://statici.behindthevoiceactors.com/behindthevoiceactors/_img/chars/katsuki-bakugo--0.99.jpg",
+      scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+      ]
+    },
+    {
+      name: "Todoroki",
+      photo: "https://images-na.ssl-images-amazon.com/images/I/51bEO6rd3zL.jpg",
+      scores: [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+      ]
+    },
+    {
+      name: "Uraraka",
+      photo: "https://data.whicdn.com/images/320413519/superthumb.jpg?t=1538800499",
+      scores: [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+    },
+    {
+      name: "yaoyorozu",
+      photo: "https://pbs.twimg.com/profile_images/990872075454832645/hNAN4_Cl_400x400.jpg",
+      scores: [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+      ]
+    },
+    {
+      name: "Himiko",
+      photo: "https://ih1.redbubble.net/image.506825317.7007/st%2Csmall%2C215x235-pad%2C210x230%2Cf8f8f8.lite-1.jpg",
+      scores: [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+      ]
+    }
+  
+  ];
+  
+module.exports = friends;
--- server.js
+++ server.js
@@ -1,6 +1,4 @@
-// Packages
 const express = require("express");
-const path = require("path");
 
 const app = express();
 
@@ -8,11 +6,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.text());
-app.use(express.json({ type: "application/vnd.api+json" }));
 
 // API and HTML routes
-//require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
 
 // Checking if server is live
